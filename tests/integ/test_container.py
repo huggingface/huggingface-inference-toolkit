51924879f39cc10da82bb5692aca094ed752680d
Philipp Schmid <schmidphilipp1995@gmail.com>
2022-07-12 21:14:09 +0000

    added prep for integration test

--- tests/integ/test_container.py
+++ tests/integ/test_container.py
@@ -1,6 +1,7 @@
 import json
 import os
 import re
+import tempfile
 import time
 
 import numpy as np
@@ -26,17 +27,18 @@ def verify_task(container: DockerClient, task: str, framework: str = "pytorch"):
     BASE_URL = "http://localhost:5000"
     model = task2model[task][framework]
     input = task2input[task]
-
     # health check
     t = 0
     while t < 10:
         try:
             response = requests.get(f"{BASE_URL}/health")
-            assert response.status_code == 200
-            break
+            if response.status_code == 200:
+                break
         except Exception:
+            pass
+        finally:
+            t += 1
             time.sleep(2)
-        t += 1
 
     prediction = requests.post(f"{BASE_URL}/predict", json=input).json()
     assert task2validation[task](result=prediction, snapshot=task2output[task]) == True
@@ -63,8 +65,19 @@ def test_cpu_container(task) -> None:
     container_name = "integration-test"
     container_image = "starlette-transformers:cpu"
     make_sure_other_containers_are_stopped(client, container_name)
-    container = client.containers.run(container_image, name=container_name, ports={"5000": "5000"}, detach=True)
-    # time.sleep(5)
-    verify_task(container, task)
-    container.stop()
-    container.remove()
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # https://github.com/huggingface/infinity/blob/test-ovh/test/integ/utils.py
+        container = client.containers.run(
+            container_image,
+            name=container_name,
+            ports={"5000": "5000"},
+            environment={"HF_MODEL_ID": "/opt/huggingface/model", "HF_TASK": task},
+            volumes={tmpdirname: {"bind": "/opt/huggingface/model", "mode": "rw"}},
+            detach=True,
+            # GPU
+            # device_requests=[docker.types.DeviceRequest(count=-1, capabilities=[["gpu"]])]
+        )
+        # time.sleep(5)
+        verify_task(container, task)
+        container.stop()
+        container.remove()
