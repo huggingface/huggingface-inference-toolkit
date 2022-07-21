8bc1182560871fb8992cd9590628fc63830fe0c8
Philipp Schmid <schmidphilipp1995@gmail.com>
2022-07-21 14:42:43 +0000

    added flake exception

--- src/huggingface_inference_toolkit/serialization/__init__.py
+++ src/huggingface_inference_toolkit/serialization/__init__.py
@@ -1,3 +1,3 @@
-from huggingface_inference_toolkit.serialization.audio_utils import Audioer
-from huggingface_inference_toolkit.serialization.image_utils import Imager
-from huggingface_inference_toolkit.serialization.json_utils import Jsoner
+from huggingface_inference_toolkit.serialization.audio_utils import Audioer  # noqa: F401
+from huggingface_inference_toolkit.serialization.image_utils import Imager  # noqa: F401
+from huggingface_inference_toolkit.serialization.json_utils import Jsoner  # noqa: F401
