f5d18b81bb5208a7a0c80d92fa31c5fa5f541941
philschmid <schmidphilipp1995@gmail.com>
2022-08-24 07:07:10 +0000

    make quality

--- src/huggingface_inference_toolkit/utils.py
+++ src/huggingface_inference_toolkit/utils.py
@@ -6,8 +6,8 @@ from pathlib import Path
 from typing import Optional, Union
 
 from huggingface_hub import HfApi
-from huggingface_hub.utils import filter_repo_objects
 from huggingface_hub.file_download import cached_download, hf_hub_url
+from huggingface_hub.utils import filter_repo_objects
 from transformers import pipeline
 from transformers.file_utils import is_tf_available, is_torch_available
 from transformers.pipelines import Conversation, Pipeline
