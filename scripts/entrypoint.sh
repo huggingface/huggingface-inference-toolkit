61d80a34c49689893412896d8ec208a333024ec1
Philipp Schmid <schmidphilipp1995@gmail.com>
2023-03-08 15:59:22 +0000

    added entrypoint for pip install if exists

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,13 @@
+# /bin/bash
+
+# check if HF_MODEL_DIR is set and if not skip installing custom dependencies
+if [ -z "${HF_MODEL_DIR}" ]; then 
+  # check if requirements.txt exists and if so install dependencies
+  if [ -f "${HF_MODEL_DIR}/requirements.txt" ]; then
+    echo "Installing custom dependencies from ${HF_MODEL_DIR}/requirements.txt"
+    pip install -r ${HF_MODEL_DIR}/requirements.txt --no-cache-dir;
+  fi
+fi
+
+# start the server
+uvicorn webservice_starlette:app --host 0.0.0.0 --port 5000
