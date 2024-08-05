c163e56f58a031d291aec2e2f9fa2a7cb6a1601c
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2024-08-05 16:33:03 +0200

    Update `setup_logging` (WIP)
    - Logs are showing in the Logs Explorer
    - Logs are too verbose (healthcheck is printed every N ms)
    - POST i.e. inference logs are nice, but those show within multiple
    lines (maybe due to `rich`)

--- src/huggingface_inference_toolkit/logging.py
+++ src/huggingface_inference_toolkit/logging.py
@@ -1,27 +1,39 @@
 import logging
 import sys
 
+from rich.console import Console
+from rich.logging import RichHandler
+
 
 def setup_logging():
-    # Remove all existing handlers
-    for handler in logging.root.handlers[:]:
-        logging.root.removeHandler(handler)
+    # # Remove all existing handlers
+    # for handler in logging.root.handlers[:]:
+    #     logging.root.removeHandler(handler)
 
     # Configure the root logger
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
-        stream=sys.stdout,
+        handlers=[
+            RichHandler(
+                rich_tracebacks=True,
+                tracebacks_show_locals=True,
+                console=Console(stderr=False, file=sys.stdout),
+            )
+        ],
     )
+    # Remove `datasets` logger to only log on `critical` mode
+    # as it produces `PyTorch` messages to update on `info`
+    logging.getLogger("datasets").setLevel(logging.CRITICAL)
 
-    # Remove Uvicorn loggers
-    logging.getLogger("uvicorn").handlers.clear()
-    logging.getLogger("uvicorn.access").handlers.clear()
-    logging.getLogger("uvicorn.error").handlers.clear()
+    # # Remove Uvicorn loggers
+    # logging.getLogger("uvicorn").handlers.clear()
+    # logging.getLogger("uvicorn.access").handlers.clear()
+    # logging.getLogger("uvicorn.error").handlers.clear()
 
-    # Create a logger for your application
     logger = logging.getLogger("huggingface_inference_toolkit")
+    logger.setLevel(logging.INFO)
     return logger
 
 
