09e8d66f69986b4d1ec528d6fdda4951572b1e56
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2024-08-08 09:22:14 +0200

    Fix `HF_TRUST_REMOTE_CODE` bool-handling via `strtobool`
    The `strtobool` had to be defined within `huggingface_inference_toolkit`
    since it's deprecated and removed from `distutils` from Python 3.10
    onwards.

--- src/huggingface_inference_toolkit/const.py
+++ src/huggingface_inference_toolkit/const.py
@@ -1,13 +1,15 @@
 import os
 from pathlib import Path
 
+from huggingface_inference_toolkit.utils import strtobool
+
 HF_MODEL_DIR = os.environ.get("HF_MODEL_DIR", "/opt/huggingface/model")
 HF_MODEL_ID = os.environ.get("HF_MODEL_ID", None)
 HF_TASK = os.environ.get("HF_TASK", None)
 HF_FRAMEWORK = os.environ.get("HF_FRAMEWORK", None)
 HF_REVISION = os.environ.get("HF_REVISION", None)
 HF_HUB_TOKEN = os.environ.get("HF_HUB_TOKEN", None)
-HF_TRUST_REMOTE_CODE = os.environ.get("HF_TRUST_REMOTE_CODE", False)
+HF_TRUST_REMOTE_CODE = strtobool(os.environ.get("HF_TRUST_REMOTE_CODE", "0"))
 # custom handler consts
 HF_DEFAULT_PIPELINE_NAME = os.environ.get("HF_DEFAULT_PIPELINE_NAME", "handler.py")
 # default is pipeline.PreTrainedPipeline
