ab791301c4fe5443924f7731395472df863a7d12
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2024-08-12 16:36:17 +0200

    Fix `sentence-transformers` unit tests

--- src/huggingface_inference_toolkit/sentence_transformers_utils.py
+++ src/huggingface_inference_toolkit/sentence_transformers_utils.py
@@ -16,7 +16,9 @@ class SentenceSimilarityPipeline:
         self.model = SentenceTransformer(model_dir, device=device)
 
     def __call__(self, inputs=None):
-        embeddings1 = self.model.encode(inputs["source_sentence"], convert_to_tensor=True)
+        embeddings1 = self.model.encode(
+            inputs["source_sentence"], convert_to_tensor=True
+        )
         embeddings2 = self.model.encode(inputs["sentences"], convert_to_tensor=True)
         similarities = util.pytorch_cos_sim(embeddings1, embeddings2).tolist()[0]
         return {"similarities": similarities}
@@ -47,12 +49,7 @@ SENTENCE_TRANSFORMERS_TASKS = {
 }
 
 
-def get_sentence_transformers_pipeline(
-    task=None,
-    model_dir=None,
-    device=-1,
-    **kwargs
-):
+def get_sentence_transformers_pipeline(task=None, model_dir=None, device=-1, **kwargs):
     device = "cuda" if device == 0 else "cpu"
-    pipeline = SENTENCE_TRANSFORMERS_TASKS[task](model_dir=model_dir, device=device, **kwargs)
+    pipeline = SENTENCE_TRANSFORMERS_TASKS[task](model_dir=model_dir, device=device)
     return pipeline
