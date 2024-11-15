c1d519a1abe3ca2c0b7876807570fb6399a06823
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2024-11-15 10:10:53 +0100

    Fix failing `sentence-transformers` tests due to input parsing

--- tests/unit/test_sentence_transformers.py
+++ tests/unit/test_sentence_transformers.py
@@ -15,9 +15,7 @@ from huggingface_inference_toolkit.utils import (
 @require_torch
 def test_get_sentence_transformers_pipeline():
     with tempfile.TemporaryDirectory() as tmpdirname:
-        storage_dir = _load_repository_from_hf(
-            "sentence-transformers/all-MiniLM-L6-v2", tmpdirname
-        )
+        storage_dir = _load_repository_from_hf("sentence-transformers/all-MiniLM-L6-v2", tmpdirname)
         pipe = get_pipeline("sentence-embeddings", storage_dir.as_posix())
         assert isinstance(pipe, SentenceEmbeddingPipeline)
 
@@ -25,9 +23,7 @@ def test_get_sentence_transformers_pipeline():
 @require_torch
 def test_sentence_embedding_task():
     with tempfile.TemporaryDirectory() as tmpdirname:
-        storage_dir = _load_repository_from_hf(
-            "sentence-transformers/all-MiniLM-L6-v2", tmpdirname
-        )
+        storage_dir = _load_repository_from_hf("sentence-transformers/all-MiniLM-L6-v2", tmpdirname)
         pipe = get_sentence_transformers_pipeline("sentence-embeddings", storage_dir.as_posix())
         res = pipe("Lets create an embedding")
         assert isinstance(res["embeddings"], list)
@@ -36,11 +32,9 @@ def test_sentence_embedding_task():
 @require_torch
 def test_sentence_similarity():
     with tempfile.TemporaryDirectory() as tmpdirname:
-        storage_dir = _load_repository_from_hf(
-            "sentence-transformers/all-MiniLM-L6-v2", tmpdirname
-        )
+        storage_dir = _load_repository_from_hf("sentence-transformers/all-MiniLM-L6-v2", tmpdirname)
         pipe = get_sentence_transformers_pipeline("sentence-similarity", storage_dir.as_posix())
-        res = pipe({"source_sentence": "Lets create an embedding", "sentences": ["Lets create an embedding"]})
+        res = pipe(**{"source_sentence": "Lets create an embedding", "sentences": ["Lets create an embedding"]})
         assert isinstance(res["similarities"], list)
 
 
