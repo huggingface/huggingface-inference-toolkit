f7f397a0606e5573ebdf2f9b042dc666dd83f885
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-04-01 11:22:55 +0200

    Move `kenlm` patched installation to `setup.py` instead

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ VERSION = "0.5.4"
 # libavcodec-extra : libavcodec-extra  includes additional codecs for ffmpeg
 
 install_requires = [
+    # Due to an error affecting kenlm and cmake (see https://github.com/kpu/kenlm/pull/464)
+    # Also see the transformers patch for it https://github.com/huggingface/transformers/pull/37091
+    "kenlm@git+https://github.com/kpu/kenlm@ba83eafdce6553addd885ed3da461bb0d60f8df7",
     "transformers[sklearn,sentencepiece,audio,vision]==4.48.0",
     "huggingface_hub[hf_transfer]==0.27.1",
     # vision
